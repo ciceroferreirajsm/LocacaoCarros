6b4b96f0f4463bca641c8d07d4aed68aa89514f4
Cicero ferreira <ciceronascimentu@gmail.com>
2020-12-16 21:30:52 -0300

    Adicionar arquivos de projeto.

---
+++ LocaçãoCarros/Entities/Invoice.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+namespace LocaçãoCarros.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return "Basic Payment: "
+                + BasicPayment.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTax: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTotal payment: "
+                + TotalPayment.ToString("D2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
---
+++ LocaçãoCarros/Services/ITaxService.cs
@@ -0,0 +1,7 @@
+﻿namespace LocaçãoCarros.Services
+{
+    interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}
